a59ca9d73d4125b755e3d820909e36b3b32bae4f
manuelr417 <manuel.rodriguez7@upr.edu>
2020-04-07 07:03:32 -0400

    acurracy

---
+++ ths/test/testcnnw1D_GPU.py
@@ -0,0 +1,19 @@
+from ths.nn.sequences.processcnnw1d import ProcessTweetsCNN1D
+import tensorflow as tf
+def main():
+    print("Working:")
+    #P = ProcessTweetsWord2VecOnePass2DCNNv2_1("data/cleantextlabels3.csv", "trained/embedding3.csv")
+    P = ProcessTweetsCNN1D("data/cleantextlabels7.csv", "data/glove.6B.50d.txt")
+    #P = ProcessTweetsWord2VecTwoPassLSTMv2_1("data/cleantextlabels4.csv", "trained/embedding3-50d.csv")
+
+    #Bueno el model12cnnv2
+    # Excelente el de modellstmatt1 con attention
+    # El mejor fue modellstmatt2 con attention
+    # also good modellstmatt3
+    # el 4 con dropout
+    # 2, 3 y 4 son buenos
+    P.process("trained/modelcnnincepw6.json", "trained/modelcnnincepw6.h5", plot=False, epochs=16)
+#joderme
+if __name__ == "__main__":
+    with tf.device('/GPU:1'):
+        main()
