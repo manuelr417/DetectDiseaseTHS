e798d9ad879b7a26533cf29e2a18842bf568fb08
manuelr417 <manuel.rodriguez7@upr.edu>
2020-04-07 18:48:37 -0400

    Add leaky relu

--- ths/test/testcnnw1D.py
+++ ths/test/testcnnw1D.py
@@ -3,7 +3,9 @@ from ths.nn.sequences.processcnnw1d import ProcessTweetsCNN1D
 def main():
     print("Working:")
     #P = ProcessTweetsWord2VecOnePass2DCNNv2_1("data/cleantextlabels3.csv", "trained/embedding3.csv")
-    P = ProcessTweetsCNN1D("data/cleantextlabels7.csv", "data/glove.6B.50d.txt")
+    #P = ProcessTweetsCNN1D("data/cleantextlabels7.csv", "data/glove.6B.50d.txt")
+    P = ProcessTweetsCNN1D("data/cleantextlabels7.csv", "data/glove.6B.100d.txt")
+
     #P = ProcessTweetsWord2VecTwoPassLSTMv2_1("data/cleantextlabels4.csv", "trained/embedding3-50d.csv")
 
     #Bueno el model12cnnv2
@@ -12,7 +14,7 @@ def main():
     # also good modellstmatt3
     # el 4 con dropout
     # 2, 3 y 4 son buenos
-    P.process("trained/modelcnnincepw6.json", "trained/modelcnnincepw6.h5", plot=True, epochs=16)
+    P.process("trained/modelcnnincepw6.json", "trained/modelcnnincepw6.h5", plot=True, epochs=16, vect_dimensions=100)
 #joderme
 if __name__ == "__main__":
     main()
